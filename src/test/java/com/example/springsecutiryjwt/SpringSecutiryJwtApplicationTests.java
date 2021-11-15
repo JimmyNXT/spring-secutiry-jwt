711a8433afc8fc83a7f079546ee45006732cdf16
Dean Minnie <dminnie33@gmail.com>
2021-11-15 16:49:27 +0200

    Initial commit

---
+++ src/test/java/com/example/springsecutiryjwt/SpringSecutiryJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecutiryjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutiryJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
